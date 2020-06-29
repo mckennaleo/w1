3fd988a5a41ebfa6f992b6774a435a8e38f0ad38
mckennaleo <mckenna.leo@gmail.com>
2020-06-29 18:58:25 +0000

    lotide refactor 6

--- lotide/test/tailTest.js
+++ lotide/test/tailTest.js
@@ -1,3 +1,11 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-assertEqual(tail([1, 2, 3]), 3);
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.strictEqual(tail([1, 2, 3]), 3);
+  });
+  it("returns 3 for [3]", () => {
+    assert.strictEqual(tail([3]), 3);
+  });
+});
