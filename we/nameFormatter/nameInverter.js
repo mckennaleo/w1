04c6e1d2a1a05d5731179fb8f05787c4f8f6fd01
mckennaleo <mckenna.leo@gmail.com>
2020-06-29 14:10:26 +0000

    nameInverter refactor

--- we/nameFormatter/nameInverter.js
+++ we/nameFormatter/nameInverter.js
@@ -1,8 +1,8 @@
 const nameInverter = function(name) {
-  if (name === undefined) {
+  if (name === undefined || name === null) {
     return "throw 'Error'";
   }
-  const usedName = name.replace(/^\s+|\s+$/g, "");
+  const usedName = name.replace(/^\s+|\s+$/g, ""); //regEx trim method
 
   if (usedName === "Mr." || usedName === "Mrs." || usedName === "Ms." || usedName === "Dr." || usedName === "") {
     return "";
@@ -11,21 +11,14 @@ const nameInverter = function(name) {
     return usedName;
   }
   const nameWords = usedName.split(' ');
-  let newNameArray = [];
-  let newName = "";
   if (!usedName.includes('.')) {
-    newNameArray = [nameWords[1], nameWords[0]];
-    newName = newNameArray.join(', ');
-    return newName;
+    return [nameWords[1], nameWords[0]].join(', ');
   }
   if (usedName.includes('.') && nameWords.length === 2) {
     return usedName;
   }
   if (usedName.includes('.') && nameWords.length === 3) {
-    newNameArray = [nameWords[0], nameWords[2] + ',' ,nameWords[1]];
-    newName = newNameArray.join(' ');
-    return newName;
+    return [nameWords[0], nameWords[2] + ',' ,nameWords[1]].join(' ');
   }
 };
-
 module.exports = nameInverter;
